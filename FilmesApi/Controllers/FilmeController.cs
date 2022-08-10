0fb5f8c9571d08730528439b19816bd0d1c51a37
jorgejunior618 <jorgejunior618@gmail.com>
2022-08-10 19:20:25 -0300

    Inicio da Controller dos filmes

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme);
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo Título é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatorio")]
+        public string Diretor { get; set; }
+
+        [Required(ErrorMessage = "O campo Gênero é obrigatorio")]
+        public string Genero { get; set; }
+
+        [Required]
+        [Range(1, 480, ErrorMessage = "A Duração deve ser informada e no maximo de 480 min (8 horas)")]
+        public int Duracao { get; set; }
+    }
+}
