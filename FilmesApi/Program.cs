52ed7ad5bea38ca5f15112667953bc7036f393fa
jorgejunior618 <jorgejunior618@gmail.com>
2022-08-11 08:55:48 -0300

    Adicionando conexão com banco de dados ao projeto

--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -1,4 +1,9 @@
+using FilmesApi.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
 // Add services to the container.
 
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseNpgsql(connectionString)) ;
 
 var app = builder.Build();
 
